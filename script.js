304751302467d42a445955ecc44e8cb33510f087
Lucas Reis <lucassantanareis.9@gmail.com>
2025-11-26 22:00:34 -0300

    Nova UI, agora não tem mais modo manual

--- script.js
+++ script.js
@@ -1,130 +1,78 @@
-// Simulação inicial 
+// Simulação inicial
 //TODO substituir por leitura do ESP32 futuramente)
 dados = {
-    atual: 32,
-    externa: 35,
-    sensacao: 35,
-    orvalho: 10,
-    umidade: 2,
-    max: 30,
-    avg: 20,
-    min: 10,
-    rpm_value: 3000,
-    modo:"Manual",     //Automático | Manual
-    temp_target:"35", // 15, 20, 25 , 30, 35, 40
-    rpm_mode:"Médio", // Automático, Máximo, Médio, Mínimo
-
+  atual: 0,
+  externa: 0,
+  sensacao: 0,
+  orvalho: 0,
+  umidade: 0,
+  max: 0,
+  avg: 0,
+  min: 0,
+  rpm_value: 3000,
 };
 
-//Espera o html carregar
-document.addEventListener('DOMContentLoaded', initApp);
+// Espera o html carregar
+document.addEventListener("DOMContentLoaded", initApp);
 
 // Função principal
 function initApp() {
+  atualizarDados();
+  atualizarData();
 
-const saveButton = document.querySelector('.save-button');
-const resetButton = document.querySelector('.reset-button');
-
-// Eventos
-saveButton.addEventListener('click', handleSave);
-resetButton.addEventListener('click', handleReset);
-
-// Listener
-document.getElementById("mode-dropdown")
-    .addEventListener("change", () => toggleDropdown());
-
-toggleDropdown();
-atualizarDados();
-atualizarData();
-
-//Atualiza a cada 1 segundo
-setInterval(atualizarData, 1000);
-setInterval(atualizarDados, 1000)
-
-}
-
-
-// Manipuladores de evento
-
-function handleSave() {
-    // TODO: salvar alterações no backend / ESP32
-    alert('Configurações salvas com sucesso!');
-}
-
-function handleReset() {
-    if (confirm('Deseja realmente restaurar as configurações de fábrica?')) {
-        // TODO: restaurar para as configurações padrão
-        alert('Configurações restauradas!');
-    }
-}
-
-function handleGet() {
-    // TODO: acessa e define os componentes com as escolhas já salvas
-}
-
-function toggleDropdown(){
-    let condition = Boolean(document.getElementById("mode-dropdown").value === "Automático")
-    let dropdowns = ["temp-dropdown", "rpm_power-dropdown"]
-
-    for (const id of dropdowns) {
-        document.getElementById(id).disabled = condition;
-    }
-
-    //TODO fazer ir pro default
-    
-}
-
-function atualizarConfiguracao(){
-    toggleDropdown()
-    //TODO rever essa prr
-    if (metricas['dropdown']['mode-dropdown'] == "Manual") {
-
-        for (const [id, valor] of Object.entries(metricas['dropdown'])) {
-            const elemento = document.getElementById(id);
-            if (elemento) elemento.value = valor;
-        }
-    }
-
-    else {
-        //coloca os valores default e impossibilita a edição dos dropdown
-    }
+  // Atualiza a cada 1 segundo
+  setInterval(() => {
+    atualizarDados();
+    atualizarStatus();
+    atualizarData();
+  }, 1000);
 
 }
 
 // Atualização de UI
 function atualizarDados() {
-    const metricas = {
-        "normal": {
-            "temp-atual": `${dados.atual}° C`,
-            "temp-externa": `${dados.externa}° C`,
-            "sensacao-termica": `${dados.sensacao}° C`,
-            "ponto-orvalho": `${dados.orvalho}° C`,
-            "umidade-relativa": `${dados.umidade} %`,
-            "max-temp": `${dados.max}° C`,
-            "avg-temp": `${dados.avg}° C`,
-            "min-temp": `${dados.min}° C`,
-            "rpm": `${dados.rpm_value} RPM`
-        },
-        "dropdown": {
-            "mode-dropdown":`${dados.modo}`,
-            "temp-dropdown":`${dados.temp_target}`,
-            "power-dropdown":`${dados.rpm_mode}`
-        },
-    };
-
-    //Carrega dados sobre temperatura e valor de rpm
-    for (const [id, valor] of Object.entries(metricas['normal'])) {
-            const elemento = document.getElementById(id);
-            if (elemento) elemento.textContent = valor;
-        }
+  const metricas = {
+    "temp-atual": `${dados.atual}° C`,
+    "temp-externa": `${dados.externa}° C`,
+    "sensacao-termica": `${dados.sensacao}° C`,
+    "ponto-orvalho": `${dados.orvalho}° C`,
+    "umidade-relativa": `${dados.umidade} %`,
+    "max-temp": `${dados.max}° C`,
+    "avg-temp": `${dados.avg}° C`,
+    "min-temp": `${dados.min}° C`,
+    rpm: `${dados.rpm_value} RPM`,
+  };
+  //Carrega dados sobre temperatura e valor de rpm
+  for (const [id, valor] of Object.entries(metricas)) {
+    const elemento = document.getElementById(id);
+    if (elemento) elemento.textContent = valor;
+  }
 }
 
-//TODO ver loop
 function atualizarData() {
-    const now = new Date()
+  const now = new Date();
 
-    const frase = now.toLocaleDateString() + ' - ' + now.toLocaleTimeString() ;
+  const frase = now.toLocaleDateString() + " - " + now.toLocaleTimeString();
 
-    const elemento = document.querySelector('.timestamp');
-    if (elemento) elemento.textContent = frase;
+  const elemento = document.querySelector(".timestamp");
+  if (elemento) elemento.textContent = frase;
+}
+
+function atualizarStatus() {
+  const indicator = document.getElementById("status-indicator");
+  const msg = document.getElementById("status-msg");
+
+  if (!indicator || !msg) return;
+
+  // TODO alterar regras corporativas de diagnóstico
+  if (dados.atual >= 35) {
+    indicator.className = "status-indicator error";
+    msg.textContent = "Sobrecarga térmica. Cooler ativado!";
+  } else if (dados.rpm_value < 500) {
+    indicator.className = "status-indicator warn";
+    msg.textContent = "RPM insuficiente. Eficiência degradada";
+  } else {
+    indicator.className = "status-indicator ok";
+    msg.textContent = "Sistemas em Funcionamento Normal!";
+  }
 }
